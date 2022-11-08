3874797cba79a7821f3805120e66671ae3e6f209
leerivera <leerivera80@gmail.com>
2022-11-08 14:57:48 -0500

    os stuff

--- app.js
+++ app.js
@@ -3,3 +3,16 @@ const os = require("os")
 
 const user = os.userInfo()
 console.log(user)
+
+// method returns system uptime
+
+console.log(`system uptime ${os.uptime()}`)
+
+const currentOS = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem()
+}
+
+console.log(currentOS)
