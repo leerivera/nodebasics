9943e7750cff53d99e471b9db6de9c703c1e7471
leerivera <leerivera80@gmail.com>
2022-11-09 15:00:52 -0500

    read file

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const http = require('http')
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/'){
-        res.end('Welcome here is my page')
-    }
-    if(req.url === '/about'){
-        res.end("here is our story it's been a long time")
-    }
-    res.end(`<h1>you're off</h1>
-    <p>you sure you're in the right place</p>
-    <a href="/">go back</a>`)
+const { readFile, writeFile } = require('fs')
+
+console.log('started a first task')
+// CHECK FILE PATH!!!!
+readFile('./content/first.txt', 'utf8', (err, result) => {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log(result)
+  console.log('completed first task')
 })
+console.log('starting next task')
 
-server.listen(5000)
+
+
+// const http = require('http')
+
+// const server = http.createServer((req, res) => {
+//     if(req.url === '/'){
+//         res.end('Welcome here is my page')
+//     }
+//     if(req.url === '/about'){
+//         res.end("here is our story it's been a long time")
+//     }
+//     res.end(`<h1>you're off</h1>
+//     <p>you sure you're in the right place</p>
+//     <a href="/">go back</a>`)
+// })
+
+// server.listen(5000)
  
 // const path = require('path')
 
