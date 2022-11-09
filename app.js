9d32a20d0ffced4f0983c90b5f82eb2a6cc748af
leerivera <leerivera80@gmail.com>
2022-11-09 15:11:23 -0500

    fs-async

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-setInterval(() => {
-    console.log('hello world')
-  }, 2000)
-  console.log(`I will run first`)
+
+
+
+
+// setInterval(() => {
+//     console.log('hello world')
+//   }, 2000)
+//   console.log(`I will run first`)
   // process stays alive unless
   // Kill Process CONTROL + C
   // unexpected error
