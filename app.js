ce3424eb59bc0c0d804f172788ddf8c707a95d3b
leerivera <leerivera80@gmail.com>
2022-11-09 15:03:20 -0500

    set timeout

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 
-const { readFile, writeFile } = require('fs')
+// started operating system process
+console.log('first')
+setTimeout(() => {
+  console.log('second')
+}, 0)
+console.log('third')
+// completed and exited operating system process
 
-console.log('started a first task')
-// CHECK FILE PATH!!!!
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  console.log(result)
-  console.log('completed first task')
-})
-console.log('starting next task')
+
+// console.log('started a first task')
+// // CHECK FILE PATH!!!!
+// readFile('./content/first.txt', 'utf8', (err, result) => {
+//   if (err) {
+//     console.log(err)
+//     return
+//   }
+//   console.log(result)
+//   console.log('completed first task')
+// })
+// console.log('starting next task')
 
 
 
